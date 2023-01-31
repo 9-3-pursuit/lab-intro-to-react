0db547b065386943b113144d3a740a92af32fb9f
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-01-31 16:36:35 -0500

    Creates Contact User Card component

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,12 @@
+export default function ContactUserCards({ classN }) {
+  return (
+    <div className={"user " + classN}>
+      <img src="profile.png" alt="profile" />
+      <div>
+        <h3>Jordan Walke</h3>
+        <p>React Creator</p>
+        <p>Lorem Ipsem</p>
+      </div>
+    </div>
+  );
+}
