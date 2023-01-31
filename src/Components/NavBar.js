7adbc496ff2f0116172e12f893080011369dcda2
HaroldF415 <harold.f415@gmail.com>
2023-01-31 16:32:10 -0500

    NavBar added

---
+++ src/Components/NavBar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <button>What is Pursuit?</button>
+      <button>Create Account</button>
+      <button>Sign in</button>
+    </nav>
+  );
+};
+export default NavBar;
