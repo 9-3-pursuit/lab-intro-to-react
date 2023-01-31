6d0977658a0a75b34efe32876514ddb3bfc567a0
HaroldF415 <harold.f415@gmail.com>
2023-01-31 16:33:31 -0500

    UserProfile complete

---
+++ src/Components/UserProfile.js
@@ -0,0 +1,14 @@
+const UserProfile = () => {
+  return (
+    <div className="profile-container">
+      <img name="" alt=""></img>
+      <div className="profile-info">
+        <h1>Title</h1>
+        <p>Subtitle</p>
+        <p>Info about the user</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
