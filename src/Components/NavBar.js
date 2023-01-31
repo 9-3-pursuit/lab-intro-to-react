8214aed33434a43bbfd183fd6873950ffe1ce22b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-31 18:32:57 -0500

    NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,20 @@
 const NavBar = () => {
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <button>What is Pursuit?</button>
-                </li>
-                <li>
-                    <button>Create an account</button>
-                </li>
-                <li>
-                    <button>Sign In</button>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <button>What is Pursuit?</button>
+                    </li>
+                    <li>
+                        <button>Create an account</button>
+                    </li>
+                    <li>
+                        <button>Sign In</button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
+export default NavBar
