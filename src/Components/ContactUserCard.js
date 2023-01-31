81efc7720afd1cf6e8090ce171d1d7546f750c06
HaroldF415 <harold.f415@gmail.com>
2023-01-31 16:43:43 -0500

    contactCard complete

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,14 @@
+const ContactUserCard = () => {
+  return (
+    <div className="contact-container">
+      <img name="" src="" alt=""></img>
+      <div className="contact-info">
+        <h1>Eva 001</h1>
+        <p>Subtitle</p>
+        <p>Contact-info</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUserCard;
