6a150e996529c2768e24e36b8c484e672d87cad5
Keeanu Rodriguez <keeanu7@gmail.com>
2023-01-31 15:27:43 -0500

    app hierarchy test done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import NavBar from "./Components/NavBar.js";
+import UserProfile from "./Components/UserProfile.js";
+
+
 import "./index.css";
 
+
 function App () {
-  return <p>Hello, world!</p>;
+  // const myNavBar = NavBar();
+  return(
+    <div class="main">
+      <NavBar/>
+      <UserProfile/>
+    </div>
+  )
 };
 
 export default App;
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,12 @@
+function UserProfile () {
+    return (
+        <div class="user">
+            <img class="img" src="./profile.png" alt="userPFP"/>
+            <p class="name">Jordan Walker</p>
+            <p class="title">React Creator</p>
+            <p class="bio">Lorem Ipsem</p>
+        </div> 
+    )
+}
+
+export default UserProfile; 
