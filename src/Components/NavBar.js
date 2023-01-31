1a7dc505460f0e9d1e3d82fad37ebd67e1637daf
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-31 18:30:13 -0500

    NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,3 +1,17 @@
 const NavBar = () => {
-    
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <button>What is Pursuit?</button>
+                </li>
+                <li>
+                    <button>Create an account</button>
+                </li>
+                <li>
+                    <button>Sign In</button>
+                </li>
+            </ul>
+        </nav>
+    </header>
 }
