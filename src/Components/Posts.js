49abb6a93f608377db12e1d05f6e7711ae56d19c
HaroldF415 <harold.f415@gmail.com>
2023-01-31 16:35:35 -0500

    Posts refactored

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,1 +1,12 @@
 import Post from "./Post";
+
+const Posts = () => {
+  return (
+    <div className="posts-container">
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+export default Posts;
