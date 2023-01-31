0a1522fb40a8417f2cd68c3a57f09e41635afea4
SangUn Park <Sangunpark1@gmail.com>
2023-01-31 14:07:16 -0500

    Finished Lab

---
+++ src/Components/UserProfile.js
@@ -0,0 +1,13 @@
+
+function UserProfile() {
+    return (
+        <div>
+            <img rel="profile" alt="profile-picture" src="../../public/profile.png"/>
+            <strong>Jordan Walke</strong>
+            <p>React Creator</p>
+            <p>Loren Ipsem</p>
+        </div>
+    );
+}
+
+export default UserProfile
