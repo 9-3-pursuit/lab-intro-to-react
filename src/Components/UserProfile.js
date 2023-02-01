3dbcc503ef9416a9196de575bb937f447244a128
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-31 19:07:18 -0500

    Add UserProfile

---
+++ src/Components/UserProfile.js
@@ -0,0 +1,17 @@
+import "./UserProfile.css"
+
+const UserProfile = () => {
+    return (
+        <div>
+            <span>
+                <img src="profile.png" alt="logo" />
+            </span>
+            <section>
+                <h1>Jordan Walke</h1>
+                <h3>React Creator</h3>
+                <p>Lorem Ipsem</p>
+            </section>
+        </div>
+    )
+}
+export default UserProfile
