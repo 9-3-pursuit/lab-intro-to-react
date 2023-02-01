695ffaf07580316672e269abb58b5a555bc945ff
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-02-01 06:38:36 -0500

    added code/data to NavBar,Contacts,Buttons,and Apps.js

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import Button from "./Button"
+import "./NavBar.css"
+
+function NavBar ()  {
+    return (
+    <div className="navbar">
+        <Button buttonText="What is Pursuit?"/>
+        <Button buttonText="Create an Account"/>
+        <Button buttonText="Sign In"/>
+        
+    </div>
+    )
+}
+
+export default NavBar 
