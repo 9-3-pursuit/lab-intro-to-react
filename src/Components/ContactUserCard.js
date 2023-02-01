ed7909de305a663a5825558a73754bf1974d5e20
pursuit-florence <florenceosei@pursuit.org>
2023-01-31 21:04:03 -0500

    My First React

---
+++ src/Components/ContactUserCard.js
@@ -0,0 +1,10 @@
+export default function ContactUserCard(){
+  return (
+    <>
+    <h2>Jordan Walke</h2>
+    <h3>React Creator</h3>
+     <p>Lorem ipsem</p>
+
+    </>
+  )
+}
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,17 @@
+
+export default function UserProfile(){
+  return(
+    <div>
+      <div>
+       <input type ="text" id="id"class="what is Pursuit" value="what is Pursuit"/>
+       <input type ="text" id="id"class="create an account" value="create an account"/>
+        <input type ="text" id="id"class="signin" value="signin"/>
+        </div>
+      <h2>Jordan Walke</h2>
+    <h3>React Creator</h3>
+     <p>Lorem ipsem</p>
+
+  </div>
+  )
+  
+}
