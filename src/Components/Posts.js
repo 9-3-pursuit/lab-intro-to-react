e363f941115b5d75a3027bf9339fd375f7f7bd43
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-31 19:04:58 -0500

    Add Posts

---
+++ src/Components/Posts.js
@@ -0,0 +1,14 @@
+import Post from "./Post"
+
+const Posts = () => {
+    return (
+    <main>
+            <h1> Posts </h1>
+            <Post />
+            <Post />
+            <Post />
+    </main>
+    )
+}
+
+export default Posts
