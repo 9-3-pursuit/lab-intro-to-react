5724b1f250f0b754d7d1f4b492c394056e4c0924
Kerridene Small <Kerridenesmall@gmail.com>
2023-02-01 08:04:54 -0500

    Completed Lab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./Components/NavBar.js";
+import Contacts from "./Components/Contacts";
+import Posts from "./Components/Posts";
+import UserProfile from "./Components/UserProfile"
 
 function App () {
-  return <p>Hello, world!</p>;
+  return (
+    <div>  
+      <NavBar /> 
+      <main> 
+        <UserProfile />
+        <Contacts />
+        <Posts />
+      </main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/UserProfile.js
@@ -0,0 +1,14 @@
+import "./UserProfile.css"
+function UserProfile (){
+    return (
+        <div className="user--profile">
+            <img src="./profile.png" alt="Jordon Walke"></img>
+            <h1>Jordan Walke</h1>
+            <p className="description">React Creator</p>
+            <p className="text"> Lorem Ipsom</p>
+
+        </div>
+    )
+}
+
+export default UserProfile
