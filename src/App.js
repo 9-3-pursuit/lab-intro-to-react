18965e3e2f0ba4a93e388403e3a98d646cfb09f0
touhami-ben <brooklyn.ben76@gmail.com>
2023-02-01 22:27:28 -0500

    passed-test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./index.css";
+import NavBar from "./Components/NavBar";
+import UserProfile from "./Components/UserProfile";
+import Posts from "./Components/Posts";
+import Contacts from "./Components/Contacts";
 
-function App () {
-  return <p>Hello, world!</p>;
+
+const App =()=> {
+  // return <p>Hello, world!</p>;
+  return (
+    <div>
+      <section>
+    <NavBar/>
+    </section>
+
+      <section>
+    <UserProfile/>
+    </section>
+
+    <main>
+    <Posts/>
+    </main>
+
+    <section>
+    <Contacts/>
+    </section>
+    </div>
+  );
+
 };
 
 export default App;
---
+++ src/Components/Contacts.js
@@ -0,0 +1,16 @@
+import ContactUserCard from "./ContactUserCard";
+
+const Contacts = ()=> {
+    return (
+    <div class="Contact">
+       <h2><strong>Contacts</strong></h2>
+        <ContactUserCard/>
+        <ContactUserCard/>
+        <ContactUserCard/>
+        <ContactUserCard/>
+
+        </div>
+    );
+};
+
+export default Contacts;
