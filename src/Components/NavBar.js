e0d60154fb36292079d148051a823b8c57e903e2
Wilton <wiltoncamilol@gmail.com>
2023-02-22 17:07:44 -0500

    final

---
+++ src/Components/NavBar.js
@@ -0,0 +1,9 @@
+export default function NavBar(){
+    return (
+        <div className="navBar">
+            <button>What is Pursuit? </button>
+            <button>Create an account </button>
+            <button>Sign in </button>
+        </div>
+    )
+}
